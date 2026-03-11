4b43e1171621083f4b73d24539cb9610efee7f10
MENY2211 <perezancerof@gmail.com>
2026-03-10 19:33:07 -0600

    Configuraciòn del gradel sin errores evidentes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,47 +1,37 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.application") version "9.1.0"
 }
 
 android {
-    namespace = "com.example.equipo0"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    namespace = "mx.tabasco.herbolaria"
+    compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.equipo0"
-        minSdk = 26
-        targetSdk = 36
+        applicationId = "mx.tabasco.herbolaria"
+        minSdk     = 26
+        targetSdk  = 35
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.3")
+    implementation("io.coil-kt:coil:2.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
